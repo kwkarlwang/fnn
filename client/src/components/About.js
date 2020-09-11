d39f74458f921800f0c2b437d67b5209fba5bcb7
Karl Wang <kawang@ucsd.edu>
2020-09-10 23:28:35 -0700

    text: add a disclaimer for the purpose of the project

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -19,16 +19,21 @@ function About() {
         <p>
           Ever since I learn about statistics in high school, I have been
           interested in the concept of "bias". Every News site, in fact, every
-          News reporter or author is biased in their report. I chose Fox News
-          and CNN not only because they are some of the most well known News
-          outlets, but also because CNN leans toward the left wing and Fox News
-          leans toward the right wing according to this{" "}
+          News reporter is biased in their report. I chose Fox News and CNN not
+          only because they are some of the most well known News outlets, but
+          also because CNN leans toward the left wing and Fox News leans toward
+          the right wing according to this{" "}
           <a href="https://www.allsides.com/media-bias/media-bias-ratings">
             AllSides article.
           </a>{" "}
           Hence, I am interested in seeing the political bias between these two
           news outlets and seeing what each of them consider as important News
           for the headlines.
+          <p>
+            Disclaimer: this project is NOT to slander, nor am I politically
+            inclined, to either News outlet, it is purely based on my personal
+            interest.
+          </p>
         </p>
       </div>
     </div>
